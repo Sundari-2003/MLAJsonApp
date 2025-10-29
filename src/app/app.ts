e8462e8afe2345af06a72fe7de2ecb34d1f89cab
Sundari Sridhar <sundari.sridhar01@gmail.com>
2025-10-29 15:29:11 +0530

    Refactor employee data and update imports
    - Changed the employee data file name from 'employees.json' to 'employeedata.json'.
    - Updated the employee data structure to include gender for each employee.
    - Removed unnecessary whitespace and formatted the JSON for better readability.
    - Added CommonModule to the imports in app.ts for improved functionality.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import employeeData from './employees.json';
+import employeeData from './employeedata.json';
 import { Employee } from './employee';
-import { DataTablesModule } from '@angular-datatables';
+import { DataTablesModule } from 'angular-datatables';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   imports: [
+    CommonModule,
     RouterOutlet,
-    DataTablesModule,
-    BrowserModule
+    DataTablesModule
+
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
